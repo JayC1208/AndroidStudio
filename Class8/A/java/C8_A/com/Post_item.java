df9acb05211605f9440866e85e23241e59d7801b
JayC1208 <43596559+JayC1208@users.noreply.github.com>
2020-04-06 00:23:20 +0900

    use glide, fragment, recyclerview, viewpager

---
+++ Class8/A/java/C8_A/com/Post_item.java
@@ -0,0 +1,46 @@
+package C8_A.com;
+
+public class Post_item {
+
+    int LikeCnt;
+    String userName, url, text;
+
+    public Post_item(int likeCnt, String userName, String url, String text) {
+        LikeCnt = likeCnt;
+        this.userName = userName;
+        this.url = url;
+        this.text = text;
+    }
+
+    public int getLikeCnt() {
+        return LikeCnt;
+    }
+
+    public void setLikeCnt(int likeCnt) {
+        LikeCnt = likeCnt;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
